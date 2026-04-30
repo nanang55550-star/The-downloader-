4c0f698b64500879166a96e5695146ff111e53d8
Nzr <nanang55550@gmail.com>
2026-04-30 15:57:43 +0700

    Update BY line formatting in utils.sh

--- utils.sh
+++ utils.sh
@@ -88,7 +88,7 @@ header() {
     echo -e "\033[1;33m║   THE DOWNLOADER v62 - UNIVERSAL     ║\033[0m"
     echo -e "\033[1;36m║   Platform: $(echo $PLATFORM | tr '[:lower:]' '[:upper:]')                 ║\033[0m"
     echo -e "\033[1;32m╚══════════════════════════════════════╝\033[0m"
-    echo -e "BY: NZR"
+    echo -e "                                                 BY: NZR"
     
     if [ "$PLATFORM" == "termux" ]; then
         AVAIL_MB=$(df /sdcard | awk 'NR==2 {printf "%.0f", $4/1024}')
